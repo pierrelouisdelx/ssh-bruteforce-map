01465cdbb3b3a4dd3909641c64f3e44b4a9ef4b0
Pierre-Louis Delcroix <pierrelouisdelcroix2001@gmail.com>
2022-07-23 20:14:37 +0200

    server port log fixed
    Signed-off-by: Pierre-Louis Delcroix <pierrelouisdelcroix2001@gmail.com>

--- server.js
+++ server.js
@@ -84,5 +84,5 @@ if (process.env.NODE_ENV === 'production') {
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
-    console.log("Server is listening on port 3001");
+    console.log("Server is listening on port " + PORT);
 });
