be9d629bd17ec87c5158e9a75c3c6f804a90e8d0
Pierre-Louis Delcroix <pierrelouisdelcroix2001@gmail.com>
2022-07-23 18:07:07 +0200

    marker added to map
    Signed-off-by: Pierre-Louis Delcroix <pierrelouisdelcroix2001@gmail.com>

--- sshreact/src/App.js
+++ sshreact/src/App.js
@@ -1,11 +1,39 @@
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+import L from 'leaflet';
 import './App.css';
+import 'leaflet/dist/leaflet.css';
+
+import marker from './img/marker.png';
+
+delete L.Icon.Default.prototype._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+    iconUrl: marker,
+    iconSize: [17.5, 20],
+    iconAnchor: [8.75, 0]
+});
 
 function App() {
-  return (
-    <div className="App">
-
-    </div>
-  );
+    const accessToken = 'robwSG8HiMYNPewxsT3oytycdnlivt23wW3EZvNk9a9V3yU6pj9QhnTtNVIovcVw';
+    const position = [51.505, -0.09]
+    return (
+        <div className="App">
+            <h1>SSH Bruteforce Map</h1>
+            <MapContainer 
+                center={[25, 0]} 
+                zoom={2} 
+                className='map'>
+                <TileLayer
+                    url={`https://{s}.tile.jawg.io/jawg-matrix/{z}/{x}/{y}.png?access-token=${accessToken}`}
+                />
+                <Marker position={position}>
+                    <Popup>
+                        A pretty CSS3 popup. <br /> Easily customizable.
+                    </Popup>
+                </Marker>
+            </MapContainer>
+        </div>
+    );
 }
 
 export default App;
